673a60d2cff4f5bc09bd5326460a1291c37e894d
nikhiljoshi009 <99590846+nikhiljoshi009@users.noreply.github.com>
2024-08-13 12:00:27 +0530

    Sg.tf

---
+++ Sg.tf
@@ -0,0 +1,51 @@
+
+resource "aws_security_group" "nj_sg_for_elb" {
+  name   = "nikhil-sg_for_elb"
+  vpc_id = aws_vpc.nj_main.id
+
+  ingress {
+    description      = "Allow http request from anywhere"
+    protocol         = "tcp"
+    from_port        = 80
+    to_port          = 80
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Allow https request from anywhere"
+    protocol         = "tcp"
+    from_port        = 443
+    to_port          = 443
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "nj_sg_for_ec2" {
+  name   = "nikhil-sg_for_ec2"
+  vpc_id = aws_vpc.nj_main.id
+
+  ingress {
+    description     = "Allow http request from Load Balancer"
+    protocol        = "tcp"
+    from_port       = 80 # range of
+    to_port         = 80 # port numbers
+    security_groups = [aws_security_group.nj_sg_for_elb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
